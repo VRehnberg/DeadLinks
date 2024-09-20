cd522018f5acb921872a55278ee433f93156450d
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2024-09-20 09:09:10 +0200

    Resolve current_url to response.url and parallelize crawl

--- src/check_site.py
+++ src/check_site.py
@@ -12,7 +12,9 @@ from tqdm.contrib.concurrent import thread_map
 
 def simplify_link(link):
     parsed_url = urlparse(link)
-    return urlunparse((parsed_url.scheme, parsed_url.netloc, parsed_url.path, '', '', ''))
+    return urlunparse(
+        (parsed_url.scheme, parsed_url.netloc, parsed_url.path, "", "", "")
+    )
 
 
 def get_links_from_page(url, timeout):
@@ -21,18 +23,23 @@ def get_links_from_page(url, timeout):
         response = requests.get(url, timeout=timeout)
     except Exception as e:
         print(f"Error fetching {url}: {e}", file=sys.stderr)
-        return [], False
+        return url, [], False
     else:
+        if response.url != url:
+            print(
+                f"WARN: Link not pointing to endpoint {url} -> {response.url}",
+                file=sys.stderr,
+            )
+            url = response.url
         if response.status_code != 200:
             print(f"Failed to retrieve {url}. Status code: {response.status_code}")
-            return [], False
+            return url, [], False
 
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
         links = set(
-            simplify_link(a_tag['href'])
-            for a_tag in soup.find_all('a', href=True)
+            simplify_link(a_tag["href"]) for a_tag in soup.find_all("a", href=True)
         )
-        return links, True
+        return url, links, True
 
 
 def is_internal_link(link, base_domain):
@@ -61,58 +68,79 @@ def should_ignore_link(link, ignore_patterns):
     return any(re.search(pattern, link) for pattern in ignore_patterns)
 
 
-def crawl_website(start_url, max_depth=2, sleep_time=0.2, timeout=5, ignore_patterns=None, verbose=False):
+def crawl_website(
+    start_url,
+    max_depth=2,
+    sleep_time=0.2,
+    timeout=5,
+    ignore_patterns=None,
+    verbose=False,
+    num_workers: int = 1,
+    progressbar: bool = False,
+):
     """Crawl the website from the start_url and check all links."""
     if ignore_patterns is None:
         ignore_patterns = []
 
     base_domain = urlparse(start_url).netloc
-    visited_pages = set()
-    pages_to_visit = [(start_url, 0)]
-    linked_pages = defaultdict(set)
 
-    while pages_to_visit:
-        current_url, depth = pages_to_visit.pop(0)
-
-        if current_url in visited_pages:
-            if verbose: print(f"Skipping already visited {current_url}")
-            continue
-
-        if depth > max_depth:
-            continue
-
-        visited_pages.add(current_url)
-
-        if verbose: print(f"Crawling: {current_url}")
-        links, success = get_links_from_page(current_url, timeout)
-        if verbose: print(f"Found {len(links)} links in {current_url}")
+    def worker(current_url: str) -> tuple[str, set[str]]:
+        """For a link retrieves the actual url and all links under base domain on that page"""
+        current_url, links, success = get_links_from_page(current_url, timeout)
+        if verbose:
+            print(f"Found {len(links)} links in {current_url}")
         if not success:
-            continue
+            return (current_url, set())
 
-        for link in links:
+        def should_return_link(link: str) -> tuple[bool, str]:
             full_link = urljoin(current_url, link)  # convert relative links to absolute
-            if verbose: print(f"{current_url} + {link} -> {full_link}")
-            
-            # Skip links that match any ignore patterns
-            if should_ignore_link(full_link, ignore_patterns):
-                if verbose: print(f"Ignoring link: {full_link}")
-                continue
-            else:
-                linked_pages[current_url].add(full_link)
+            keep_link = is_internal_link(
+                full_link, base_domain
+            ) and not should_ignore_link(full_link, ignore_patterns)
+            return keep_link, full_link
 
-            # If it's an internal link and we haven't visited it, add to the queue
-            is_internal = is_internal_link(full_link, base_domain)
-            if is_internal and full_link not in visited_pages:
-                assert full_link.endswith("/") or full_link.endswith(".html")
-                pages_to_visit.append((simplify_link(full_link), depth + 1))
+        links = set(
+            full_link
+            for link in links
+            for keep_link, full_link in [should_return_link(link)]
+            if keep_link
+        )
 
         # Sleep between requests to avoid overloading the server
         sleep(sleep_time)
+        return current_url, links
+
+    visited_pages = set()
+    pages_to_visit = [start_url]
+    linked_pages = dict()
+
+    for depth in range(max_depth + 1):
+        linked_pages.update(
+            dict(
+                thread_map(
+                    worker,
+                    pages_to_visit,
+                    desc=f"Crawling at depth {depth}",
+                    max_workers=num_workers,
+                    disable=not progressbar,
+                )
+            )
+        )
+        visited_pages = set(linked_pages.keys())
+        pages_to_visit = set.union(*linked_pages.values()) - visited_pages
+
     return linked_pages
 
 
-def check_links(linked_pages: dict[str, set], timeout: float = 5, sleep_time: float = 0.2, progressbar: bool = False, verbose: bool = False, num_workers: int = 1) -> bool:
-    '''Check for bad links, return true if all are ok.'''
+def check_links(
+    linked_pages: dict[str, set],
+    timeout: float = 5,
+    sleep_time: float = 0.2,
+    progressbar: bool = False,
+    verbose: bool = False,
+    num_workers: int = 1,
+) -> bool:
+    """Check for bad links, return true if all are ok."""
 
     def worker(link):
         valid, status_link = check_link_status(link, timeout)
@@ -127,6 +155,7 @@ def check_links(linked_pages: dict[str, set], timeout: float = 5, sleep_time: fl
             thread_map(
                 worker,
                 unique_links,
+                desc="Checking links",
                 max_workers=num_workers,
                 disable=not progressbar,
             ),
@@ -153,15 +182,46 @@ def check_links(linked_pages: dict[str, set], timeout: float = 5, sleep_time: fl
 
 def main():
     # Parse command-line arguments
-    parser = argparse.ArgumentParser(description="Crawl a website and check for broken links.")
+    parser = argparse.ArgumentParser(
+        description="Crawl a website and check for broken links."
+    )
     parser.add_argument("start_url", help="The starting URL of the website to crawl.")
-    parser.add_argument("--max-depth", type=int, default=2, help="Maximum depth when crawling (default: 2).")
-    parser.add_argument("--sleep-time", type=float, default=0.0, help="The time to sleep between requests (default: 0.0 seconds).")
-    parser.add_argument("--timeout", type=int, default=2, help="The request timeout in seconds (default: 2 seconds).")
-    parser.add_argument("--ignore", nargs="*", default=["^mailto:"], help="List of patterns (regex) to ignore when crawling.")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Increase verbosity.")
-    parser.add_argument("--progressbar", action="store_true", help="Enable progress bar.")
-    parser.add_argument("--num-workers", type=int, default=1, help="Number of threads to use (default: 1).")
+    parser.add_argument(
+        "--max-depth",
+        type=int,
+        default=2,
+        help="Maximum depth when crawling (default: 2).",
+    )
+    parser.add_argument(
+        "--sleep-time",
+        type=float,
+        default=0.0,
+        help="The time to sleep between requests (default: 0.0 seconds).",
+    )
+    parser.add_argument(
+        "--timeout",
+        type=int,
+        default=2,
+        help="The request timeout in seconds (default: 2 seconds).",
+    )
+    parser.add_argument(
+        "--ignore",
+        nargs="*",
+        default=["^mailto:"],
+        help="List of patterns (regex) to ignore when crawling.",
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Increase verbosity."
+    )
+    parser.add_argument(
+        "--progressbar", action="store_true", help="Enable progress bar."
+    )
+    parser.add_argument(
+        "--num-workers",
+        type=int,
+        default=1,
+        help="Number of threads to use (default: 1).",
+    )
 
     args = parser.parse_args()
 
@@ -173,6 +233,8 @@ def main():
         timeout=args.timeout,
         ignore_patterns=args.ignore,
         verbose=args.verbose,
+        progressbar=args.progressbar,
+        num_workers=args.num_workers,
     )
     links_ok = check_links(
         linked_pages=linked_pages,
